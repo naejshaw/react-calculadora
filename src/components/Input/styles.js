f2d31acf15fc2f90cd18d619ffbed205f44c571a
JeanFDev <jeanjfra1@gmail.com>
2023-12-14 18:06:47 -0300

    update

---
+++ src/components/Input/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: gray;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 20px;
+    font-family: 'Roboto';
+
+    input{
+        width: 100%;
+        height: 75px;
+        background-color: gray;
+        border: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        padding: 0 10px;
+        text-align: right;
+        font-size: 20px;
+        font-family: 'Roboto';
+        color: #FFFFFF;
+    }
+`
